a2017d2612319bbeb204f1d515ba9ff3d740ee4a
bogdanilchenko1987 <bogdan_ilchenko@ukr.net>
2024-12-10 23:26:35 +0200

    add sort

--- services/getAllCountries.ts
+++ services/getAllCountries.ts
@@ -1,6 +1,10 @@
-const API_URL = 'https://restcountries.com/v3.1/all';
+/* eslint-disable @typescript-eslint/no-explicit-any */
+const API_URL = "https://restcountries.com/v3.1/all";
+import { Country } from "@/types/Country";
+import { sortBy } from "ramda";
 
-export default async function getAllCountries() {
+export default async function getAllCountries(): Promise<Country[]> {
   const res = await fetch(API_URL);
-  return res.json();
+  const data = (await res.json()) as Country[];
+  return sortBy((country: any) => country.name.common, data);
 }
